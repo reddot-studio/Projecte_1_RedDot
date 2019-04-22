cf99e7f4c4892128bd36c9685598ea7bc1cf5a9f
Berniix <djberniix@gmail.com>
2019-04-22 19:41:13 +0200

    Module Text Creation

--- Project_Art_Of_Fighting/Application.cpp
+++ Project_Art_Of_Fighting/Application.cpp
@@ -13,7 +13,8 @@
 #include "ModuleDebug.h"
 #include "ModuleAudio.h"
 #include "ModuleCollision.h"
-#include"ModuleUI.h"
+#include "ModuleText.h"
+#include "ModuleUI.h"
 
 
 Application::Application()
@@ -35,7 +36,7 @@ Application::Application()
 	modules[num++] = debug = new ModuleDebug();
 	modules[num++] = collision = new ModuleCollision();
 	modules[num++] = sceneUI = new ModuleUI();
-	
+	modules[num++] = text = new ModuleText();
 	
 }	
 
---
+++ Project_Art_Of_Fighting/ModuleText.h
@@ -0,0 +1,11 @@
+#ifndef _MODULETEXT_
+#define _MODULETEXT_
+
+#include "Module.h"
+#include "Globals.h"
+
+class ModuleText : public Module {
+
+};
+
+#endif // !_MODULETEXT_
