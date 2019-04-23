2663a13be5943eedb970fd5be5ddf06d4ee6a27c
Berniix <djberniix@gmail.com>
2019-04-23 02:15:08 +0200

    Module font creation 2
    some improves

---
+++ Project_Art_Of_Fighting/ModuleText.cpp
@@ -0,0 +1,20 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleText.h"
+#include "SDL/include/SDL.h"
+
+
+ModuleText::ModuleText() {}
+ModuleText::~ModuleText() {}
+
+bool ModuleText::Start() {
+
+}
+
+update_status ModuleText::Update() {
+
+}
+
+bool ModuleText::CleanUp() {
+
+}
--- Project_Art_Of_Fighting/ModuleText.h
+++ Project_Art_Of_Fighting/ModuleText.h
@@ -5,6 +5,12 @@
 #include "Globals.h"
 
 class ModuleText : public Module {
+public:
+	ModuleText();
+	~ModuleText();
+	bool Start();
+	update_status Update();
+	bool CleanUp();
 
 };
 
